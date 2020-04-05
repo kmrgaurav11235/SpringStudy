9e4b847740ec7346bc75001f6688de8d03f7737a
Kumar Gaurav <kgaurav@adobe.com>
2020-04-05 19:15:39 +0530

    Another Impl

--- 01-spring-xml-config/src/main/java/org/gaurav/HelloSpringApp.java
+++ 01-spring-xml-config/src/main/java/org/gaurav/HelloSpringApp.java
@@ -5,16 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		
-		// Get the context
-		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
-		// Retrieve beans from context
+
+		// Get the context: Load the Configuration file
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		// Retrieve beans from context (Spring Container)
 		Coach theCoach = classPathXmlApplicationContext.getBean("theCoach", Coach.class);
-		
+
 		// Call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
-		
+
 		// Close the context
 		classPathXmlApplicationContext.close();
 	}
