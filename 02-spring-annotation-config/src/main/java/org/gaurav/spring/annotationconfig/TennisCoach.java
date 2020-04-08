4713209ae726d2d7a0fc2aa25f857d042b69c9ec
Kumar Gaurav <kmrgaurav11235@gmail.com>
2020-04-08 16:26:27 +0530

    Setter Injection

--- 02-spring-annotation-config/src/main/java/org/gaurav/spring/annotationconfig/TennisCoach.java
+++ 02-spring-annotation-config/src/main/java/org/gaurav/spring/annotationconfig/TennisCoach.java
@@ -8,17 +8,26 @@ public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
 
+	// Constructor Injection
+	/*
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
-
+	*/
+	
 	public String getDailyWorkout() {
 		return "Practice back-hand volley for 20 minutes.";
 	}
 
 	public String getFortune() {
 		return fortuneService.getDailyFortune();
+	}
+
+	// Setter Injection
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
 	}
 
 }
