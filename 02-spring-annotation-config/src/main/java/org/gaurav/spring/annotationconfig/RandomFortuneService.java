33edf5770ea7385e7b378cec9f8db60d59bbd6b8
Kumar Gaurav <kmrgaurav11235@gmail.com>
2020-04-08 16:56:55 +0530

    Qualifier for Dependency Injection

---
+++ 02-spring-annotation-config/src/main/java/org/gaurav/spring/annotationconfig/RandomFortuneService.java
@@ -0,0 +1,13 @@
+package org.gaurav.spring.annotationconfig;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+	
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- 02-spring-annotation-config/src/main/java/org/gaurav/spring/annotationconfig/TennisCoach.java
+++ 02-spring-annotation-config/src/main/java/org/gaurav/spring/annotationconfig/TennisCoach.java
@@ -1,6 +1,7 @@
 package org.gaurav.spring.annotationconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class TennisCoach implements Coach {
 	
 	// Field Injection
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 
 	// Constructor Injection
