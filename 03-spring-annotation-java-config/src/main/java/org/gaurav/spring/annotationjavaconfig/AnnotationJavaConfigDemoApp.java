93dd4adf644d8ae5866b16780d414b279ea1b0b9
Kumar Gaurav <kmrgaurav11235@gmail.com>
2020-04-10 01:02:31 +0530

    Spring configuration with Java code

--- 03-spring-annotation-java-config/src/main/java/org/gaurav/spring/annotationjavaconfig/AnnotationJavaConfigDemoApp.java
+++ 03-spring-annotation-java-config/src/main/java/org/gaurav/spring/annotationjavaconfig/AnnotationJavaConfigDemoApp.java
@@ -1,8 +1,20 @@
 package org.gaurav.spring.annotationjavaconfig;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AnnotationJavaConfigDemoApp {
 
 	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportsConfig.class);
+		
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getFortune());
+		
+		context.close();
 	}
 
 }
---
+++ 03-spring-annotation-java-config/src/main/java/org/gaurav/spring/annotationjavaconfig/SportsConfig.java
@@ -0,0 +1,9 @@
+package org.gaurav.spring.annotationjavaconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.gaurav.spring.annotationjavaconfig")
+public class SportsConfig {
+}
